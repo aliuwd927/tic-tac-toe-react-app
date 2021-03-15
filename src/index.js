03f130ce5e820007d16cad8ca4f0be6c49b60e6a
Andy Liu <aliu927@gmail.com>
2021-03-14 23:32:49 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
 
+class Sqaure extends Component{
+  render(){
+    return(
+      <button className='square'>
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Board extends Component{
+  renderSquare(i){
+    return <Square value={i}/>
+}
+
+class Game extends Component{
+  render(){
+    return(
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+
+        <div className='game-info'>
+          <div>{/* status*/}</div>
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+// ========================================
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
