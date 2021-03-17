8cf2ffed1e4739075582ec48845c2533d9b879b4
Andy Liu <aliu927@gmail.com>
2021-03-16 22:20:03 -0700

    change class to function, adde logic

--- src/index.js
+++ src/index.js
@@ -2,26 +2,36 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-class Square extends Component {
-
-  render() {
-    return (
-      <button 
-      className="square" 
-      onClick={()=>{this.props.onClick({value: 'X'})}}>
-        {this.props.value}
-      </button>
-    )
-  }
+function Square(props){
+  return (
+    <button 
+    className="square" 
+    onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
+
 
 class Board extends Component {
   constructor(props){
     super(props)
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
+
+  handleClick(i){
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X': 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    
+    })
+  }
+
 
   renderSquare(i) {
     return <Square 
@@ -31,7 +41,7 @@ class Board extends Component {
   }
 
   render(){
-    const status = 'Next player: X'
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X':'O')
     
       return(
         <div>
